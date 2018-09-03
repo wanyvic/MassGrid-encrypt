10849b3d3e9c8ae5001f922c9e28628a55ff709e
wany <873040807@qq.com>
2018-09-03 12:42:21 +0800

    build

--- test_client.py
+++ test_client.py
@@ -8,43 +8,53 @@ import json
 import errno
 import datetime
 import sys
-from massgrid_wif_ecies import MassGridencrypt
+#from massgrid_wif_ecies import MassGridencrypt
 user='wany'
 pwd='19950815'
-serverip='47.92.28.155'
-serverport=50001
+serverip='120.78.79.157'
+serverport=10001
 connectuser='zxx'
 connectuserip=''
 connectuserport=''
+dictuser={}
+dictaddr={}
 def formatjson(method,id,params):
-    data ={
+    if not method:
+        data ={
         'jsonrpc':'2.0',
-        'method' :method,
         'id'     :id,
-        'params' : params 
+        'result' : params 
+        
     }
+    else:
+            
+        data ={
+            'jsonrpc':'2.0',
+            'method' :method,
+            'id'     :id,
+            'params' : params 
+        }
     return json.dumps(data)
 def response(socket):
     data = ''  
-    while True:  
-        try:  
-            new_data = sock.recv(1024)  
-        except socket.error, e:  
-            if e.args[0] == errno.EWOULDBLOCK:  
-                break  
-            raise  
+    try:  
+        new_data = sock.recv(1024)  
+    except socket.error, e:  
+        if e.args[0] == errno.EWOULDBLOCK:  
+            print 'error'
+    else:  
+        if not new_data:
+            print 'no new data'  
         else:  
-            if not new_data:  
-                break  
-            else:  
-                print new_data  
-                data += new_data
+            print 'new_data',new_data  
+            data += new_data
     if not data:
         sock.close()  
     else:
         processResponse(data)
-def processResponse(json):
-    data=json.loads(json)
+def processResponse(jsons):
+    print 'json:',jsons
+    data=json.loads(jsons)
     if not data:
         return False
     if data['id']=='0':
@@ -60,21 +70,30 @@ def processResponse(json):
 class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         try:
-            while True:
-
-
-                self.data=self.request.recv(1024)
-                print("{} send:".format(self.client_address),self.data)
-                if not self.data:
-                    print("connection lost")
-                    break
-                self.request.sendall(self.data.upper())
+            self.data=self.request.recv(1024)
+            data=json.loads(self.data)
+            print 'recv: ',self.data
+            jsonstr=''
+            if data['id']=='0':
+                dictuser[data['params'][0]]=data['params'][1]
+                dictaddr[data['params'][0]]=self.client_address
+                jsonstr = formatjson(None,'0',True)
+            if data['id']=='1':
+                #if dictaddr
+                if data['params'][0] not in dictaddr:
+                    jsonstr = formatjson(None,'1',False)
+                else:
+                    jsonstr = formatjson(None,'1',dictaddr[data['params'][0]])
+            print 'send: ',jsonstr
+            if not self.data:
+                print("connection lost")
+            self.request.sendall(jsonstr)
         except Exception as e:
-            print(self.client_address,"连接断开")
+            print(self.client_address,"connect broke")
         finally:
             self.request.close()
     def setup(self):
-        print("before handle,连接建立：",self.client_address)
+        print("before handle,connected:",self.client_address)
         
     def finish(self):
         print("finish run  after handle")
@@ -83,27 +102,30 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--server', dest='mode', action='store_const', const='server', help='server')
     args = parser.parse_args()
     if args.mode == 'server':
-        server=SocketServer.ForkingTCPServer((serverip,serverport),MyTCPHandler)
+        print 'use server :',serverip,serverport
+        server=SocketServer.ForkingTCPServer(('',serverport),MyTCPHandler)
         server.serve_forever()
     else:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-        sock.bind(('127.0.0.1',9999))
-        sock.setblocking(0)
+        sock.bind(('',9999))
+        #sock.setblocking(0)
+        print 'connect server :',serverip,serverport
         sock.connect((serverip, serverport))
+        print 'local address :',sock.getsockname()
         while True:
-            cmd=raw_input('wait command')
+            cmd=raw_input('wait command\n')
             if cmd=='exit':
                 sys.exit()
             elif cmd =='login':
-                user=raw_input('input user')
-                pwd=raw_input('input pwd')
+                user=raw_input('input user\n')
+                pwd=raw_input('input pwd\n')
                 params=[user,pwd]
                 jsonstr = formatjson('login','0',params)
                 print 'send : {%s}'%jsonstr
                 sock.sendall(jsonstr)
             elif cmd =='getuseraddress':
-                connectuser=raw_input('input connectuser')
+                connectuser=raw_input('input connectuser\n')
                 params=[connectuser]
                 jsonstr = formatjson('getuseraddress','1',params)
                 print 'send : {%s}'%jsonstr
